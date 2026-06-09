7028de79e580d757505aa7ff690e8fdeeaabedf8
divyau9813-ui <divyau9813@gmail.com>
2026-06-09 19:39:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
+
 const mongoose = require("mongoose");
 const cors = require("cors");
 
@@ -11,14 +13,23 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/files", fileRoutes);
 
-app.get("/", (req, res) => {
-    res.send("SecureVault Server Running");
-});
 
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
